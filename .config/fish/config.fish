43b58424a2b6f65c6412bec2e9ff2a8118971688
Wout De Puysseleir <woutdp@gmail.com>
2015-08-10 10:25:43 +0200

    Added some usefull aliases to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,15 @@
 set fish_greeting ""
+
+alias gst "git status"
+alias gall "git add ."
+
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias ..... "cd ../../../.."
+
+#Faster apt-get
+alias apt 'sudo apt-get'
+
+function fuck
+    eval command sudo $history[1]
+end
