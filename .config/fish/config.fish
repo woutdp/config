a00969f450569e7b63e74f4e79a9969ba336471b
Wout De Puysseleir <woutdp@gmail.com>
2015-09-03 11:58:56 +0200

    Added usefull alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,10 @@ alias ... "cd ../.."
 alias .... "cd ../../.."
 alias ..... "cd ../../../.."
 
+alias data "cd /media/wout/Data"
+alias home "cd ~"
+alias projects "cd /media/wout/Data/Projects"
+
 #Faster apt-get
 alias apt 'sudo apt-get'
 
