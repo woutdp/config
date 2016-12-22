318160c43797ad99c7dcd023dfd725e073d2c708
Wout De Puysseleir <woutdp@gmail.com>
2016-12-22 12:55:00 +0100

    Removed unused commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,14 @@
 set fish_greeting ""
 
 alias gst "git status"
-alias gall "git add ."
-alias gcom "git commit -m"
 
 alias ... "cd ../.."
 alias .... "cd ../../.."
 alias ..... "cd ../../../.."
 
-alias data "cd /media/wout/Data"
-alias home "cd ~"
-alias projects "cd /media/wout/Data/Projects"
-
-#Faster apt-get
-alias apt 'sudo apt-get'
-
 function fuck
     eval command sudo $history[1]
 end
 
+alias go_mvne 
 source ~/.config/fish/config_local.fish
