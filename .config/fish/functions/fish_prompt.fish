37cf2d2d397f5e1a198ec04dba381b7584b0c105
Wout De Puysseleir <woutdp@gmail.com>
2018-06-02 14:28:13 +0200

    Fix indentation

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,43 +1,43 @@
 function fish_prompt
-    if not set -q -g __fish_robbyrussell_functions_defined
-        set -g __fish_robbyrussell_functions_defined
-        function _git_branch_name
-            echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-        end
+  if not set -q -g __fish_robbyrussell_functions_defined
+    set -g __fish_robbyrussell_functions_defined
+    function _git_branch_name
+      echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+      end
 
-        function _is_git_dirty
-            echo (git status -s --ignore-submodules=dirty ^/dev/null)
-        end
-    end
+      function _is_git_dirty
+        echo (git status -s --ignore-submodules=dirty ^/dev/null)
+      end
+  end
 
-	if set -q VIRTUAL_ENV
-    	echo -n -s (set_color white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-	end
+  if set -q VIRTUAL_ENV
+    echo -n -s (set_color white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  end
 
-    set_color -o green;
-    echo -n (whoami)
+  set_color -o green;
+  echo -n (whoami)
 
-    if [ -d ".git" ]
+  if [ -d ".git" ]
+    set_color -o blue;
+    echo -n ' '(prompt_last_pwd)
+
+    set_color -o purple;
+    echo -n ' {'(_git_branch_name)
+
+    if [ (_is_git_dirty) ]
+      echo -n ' *'
+      end
+      echo -n '}'
+  else
+    if git status > /dev/null 2>&1
+      set_color -o purple;
+      else
         set_color -o blue;
-        echo -n ' '(prompt_last_pwd)
+      end
+      echo -n ' '(prompt_last_pwd)
+  end
 
-        set_color -o purple;
-        echo -n ' {'(_git_branch_name)
-
-        if [ (_is_git_dirty) ]
-            echo -n ' *'
-        end
-        echo -n '}'
-    else
-        if git status > /dev/null 2>&1
-            set_color -o purple;
-        else
-            set_color -o blue;
-        end
-        echo -n ' '(prompt_last_pwd)
-    end
-
-    set_color -o red;
-    echo -e ''
-    echo 'λ '
+  set_color -o red;
+  echo -e ''
+  echo 'λ '
 end
